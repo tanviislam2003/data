3890bc4eebfe45ed8ccbcd4e0201d67216afab2f
tanviislam2003 <vabna3317@gmail.com>
2023-10-13 14:28:46 +0600

    first commit

---
+++ data.js
@@ -0,0 +1,195 @@
+const data =[
+    {
+        week_name: "Week 1",
+        engagement: "40",
+        week_data: [
+            {
+                day: "Monday",
+                activity: {
+                    task_report: "React select,Node,js",
+                    spent_hour: "8",
+                    date: "10-04-23",
+                },
+            },
+            {
+                day: "Tuesday",
+                activity: {
+                    task_report: "Node React, Js, vire Rayan vavn",
+                    spent_hour: "6",
+                    date: "11-04-23",
+                },
+            },
+            {
+                day: "Wednesday",
+                activity: {
+                    task_report: "Node React, Js, vire Rayan vavn",
+                    spent_hour: "8",
+                    date: "12-04-23",
+                },
+            },
+            {
+                day: "Thursday",
+                activity: {
+                    task_report: "Node React, Js, vire Rayan vavn",
+                    spent_hour: "7",
+                    date: "13-04-23",
+                },
+            },
+            {
+                day: "Friday",
+                activity: {
+                    task_report: "Node React, Js, vire Rayan vavn",
+                    spent_hour: "8",
+                    date: "14-04-23",
+                },
+            },
+        ],
+    },
+    {
+        week_name: "Week 2",
+        engagement: "30",
+        week_data: [
+            {
+                day: "Monday",
+                activity: {
+                    task_report: "Node React, Js, vire Rayan vavn",
+                    spent_hour: "8",
+                    date: "17-04-23",
+                },
+            },
+            {
+                day: "Tuesday",
+                activity: {
+                    task_report: "React select, Node,js",
+                    spent_hour: "6",
+                    date: "18-04-23",
+                },
+            },
+            {
+                day: "Wednesday",
+                activity: {
+                    task_report: "React select, Node,js",
+                    spent_hour: "6",
+                    date: "19-04-23",
+                },
+            },
+            {
+                day: "Thursday",
+                activity: {
+                    task_report: "React select, Node,js",
+                    spent_hour: "8",
+                    date: "20-04-23",
+                },
+            },
+            {
+                day: "Friday",
+                activity: {
+                    task_report: "React select, Node,js",
+                    spent_hour: "8",
+                    date: "21-04-23",
+                },
+            },
+        ],
+    },
+    {
+        week_name: "Week 3",
+        engagement: "30",
+        week_data: [
+           
+            {
+                day: "Monday",
+                activity: {
+                    task_report: "React select, Node,js",
+                    spent_hour: "8",
+                    date: "24-04-23",
+
+            }
+        },
+        {
+            
+            day: "Tuesday",
+            activity: {
+                task_report: "React select, Node,js",
+                spent_hour: "7",
+                date: "25-04-23",
+            }
+        },
+        {
+            day: "Wednesday",
+            activity: {
+                task_report: "React select, Node,js",
+                spent_hour: "8",
+                date: "26-04-23",
+            }
+        },
+       {
+        day: "Thursday",
+        activity: {
+            task_report: "React select, Node,js",
+            spent_hour: "8",
+            date: "27-04-23",
+        }
+
+       },
+       {
+        day: "Friday",
+        activity: {
+            task_report: "React select, Node,js",
+            spent_hour: "8",
+            date: "28-04-23",
+        }
+
+       },
+        ],
+    },
+    {
+        week_name: "Week 4",
+        engagement: "20",
+        week_data: [
+            {
+                day: "Monday",
+                activity: {
+                    task_report: "React select, Node,js",
+                    spent_hour: "8",
+                    date: "01-05-23",
+                }
+        
+            },
+            {
+                day: "Tuesday",
+                activity: {
+                    task_report: "React select, Node,js",
+                    spent_hour: "8",
+                    date: "03-05-23",
+                }
+            },
+            {
+                day: "Wednesday",
+                activity: {
+                    task_report: "React select, Node,js",
+                    spent_hour: "8",
+                    date: "04-05-23",
+                }
+                
+            },
+           {
+            day: "Thursday",
+            activity: {
+                task_report: "React select, Node,js",
+                spent_hour: "8",
+                date: "05-05-23",
+            }
+           },
+        {
+            day: "Friday",
+            activity: {
+                task_report: "React select,Node,js,",
+                spent_hour: "7",
+                date: "06-05-23",
+  
+          }
+        },
+
+        ],
+    },
+];
